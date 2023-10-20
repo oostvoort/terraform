9e5825369d1af37f847248dac61ca3aeed8a9e73
miko-ov <miko@oostvoort.com>
2023-10-20 15:37:44 +0800

    Apply fix to AZ of subnets

--- aws/modules/vpc/main.tf
+++ aws/modules/vpc/main.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "my_subnet" {
   vpc_id = aws_vpc.my_vpc.id
   cidr_block = var.subnet_cidrs[count.index]
   availability_zone = element(
-    flatten([for az in data.aws_availability_zones.available.names : [az] * length(var.subnet_cidrs)]), 
+    flatten([for az in data.aws_availability_zones.available.names : [for _ in var.subnet_cidrs : az]]),
     count.index
   )
 
