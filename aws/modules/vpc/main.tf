bcee0da1573a1bd6c23f353ca89bf3666901becf
miko-ov <miko@oostvoort.com>
2023-10-23 13:44:05 +0800

    add egress to security group

--- aws/modules/vpc/main.tf
+++ aws/modules/vpc/main.tf
@@ -45,6 +45,12 @@ resource "aws_security_group" "my_security_group" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
   Name = "${var.projectname}-${var.environment}-sg"
